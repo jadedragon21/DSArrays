dd1ed2753bb8d9441a2ed80139c30bf641575d94
Sarah Hoffman <74530173+jadedragon21@users.noreply.github.com>
2021-01-13 17:00:41 -0600

    #2 Commit
    Thanks to Erik spotting the fact it wasn't all in one we realized number two was its own separate code to fix.

--- src/com/tts/Main.java
+++ src/com/tts/Main.java
@@ -4,6 +4,28 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        //broken code #2
+        double[] exampleArray = {1, 5, 6, 5, 4, 1};
+
+        double maximum = exampleArray[0];
+
+        int index = 0;
+
+        for (int i = 1; i < exampleArray.length; i++) {
+
+            if (exampleArray[i] > maximum) {
+
+                maximum = exampleArray[i];
+
+                index = i;
+
+            }
+
+        }
+        System.out.println(index);
+
+
+        //Sum down below #1
         int[] dblAyaya = {1, 5, 6, 5, 4, 1};
         int sum = 0;
         for (int i : dblAyaya) {
@@ -16,6 +38,8 @@ public class Main {
         System.out.print("Your result is: " + Arrays.toString(result));
     }
 
+
+    //math.pow #3
     public static double[] toPower(int size, int power) {
         double[] result = new double[size];
         for (int i = 0; i < size; i++) {
